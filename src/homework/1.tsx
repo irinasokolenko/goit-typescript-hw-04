c67d5cee84cdc41cbf0fd9fd5a0122bc242535e1
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-11-06 15:23:25 +0200

    hw04

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,13 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+/*
+  У вас є компонент React, який використовує useRef та IntersectionObserver для визначення, 
+ коли користувач переглядає кінець вмісту. Ваше завдання полягає в наступному:
+  Встановіть правильні типи пропсів для цього компонента. У ньому є дві властивості:
+ children і onContentEndVisible. children - це будь-який валідний React вузол, 
+  а onContentEndVisible - це функція без аргументів, що повертає void.
+  Встановіть правильний тип useRef. Посилання endContentRef використовується для div,
+ який міститься в кінці вмісту.
+  Встановіть правильний тип для options (клас також може бути типом для options).
+
+```ts
+*/
+
+import React, { ReactNode, useEffect, useRef } from 'react';
 
 // Опишіть Props
+type Props = {
+  children: ReactNode;
+  onContentEndVisible: () => void
+}
+
+type Options = {
+  rootMargin: string
+  threshold: number
+  root: Element | null
+}
+
 export function Observer({ children, onContentEndVisible }: Props) {
   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+  const endContentRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
+    const options: Options = {
       rootMargin: '0px',
       threshold: 1.0,
       root: null,
